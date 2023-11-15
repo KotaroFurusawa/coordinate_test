5154730dcaac0a58709197b28e41a92b766db668
KotaroFurusawa <k-furusawa-3x8@eagle.sophia.ac.jp>
2023-11-15 18:54:56 +0900

    fix map

--- src/Map.js
+++ src/Map.js
@@ -1,22 +1,37 @@
-import L from 'leaflet'
-import { MapContainer, TileLayer, Marker} from 'react-leaflet';
-
-// const customIcon = new L.Icon({
-//   iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png', // 任意の色のアイコンURL
-//   iconSize: [25, 41],
-//   // iconAnchor: [12, 41],
-//   // popupAnchor: [1, -34],
-//   // shadowSize: [41, 41],
-// });
-
+import React, { useRef, useEffect } from 'react';
+import L from 'leaflet';
 
 const MapWithCurrentLocation = ({ position, height }) => {
-  return (
-    <MapContainer key={`${position[0]}-${position[1]}`} center={position} zoom={20} style={{ height }}>
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      {position && <Marker position={position} />}
-    </MapContainer>
-  );
+  const mapRef = useRef(null);
+  const markerRef = useRef(null);
+
+  useEffect(() => {
+    if (!mapRef.current) {
+      const map = L.map('map').setView(position, 20);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; OpenStreetMap contributors',
+      }).addTo(map);
+
+      mapRef.current = map;
+
+      const customIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+        iconSize: [25, 41],
+      });
+
+      markerRef.current = L.marker(position, { icon: customIcon }).addTo(map);
+    }
+  }, [position]);
+
+  useEffect(() => {
+    if (markerRef.current && position) {
+      markerRef.current.setLatLng(position);
+      mapRef.current.setView(position);
+    }
+  }, [position]);
+
+  return <div id="map" style={{ height }}></div>;
 };
 
 export default MapWithCurrentLocation;
